1535e701c976dfe8346e221d8fa5dfc7e43814e6
Adrian Mendez <adrianmndz328@gmail.com>
2021-01-31 17:30:26 -0600

    Minimum Depth of Binary Tree Solution

---
+++ python/min_depth.py
@@ -0,0 +1,12 @@
+    def minDepth(self, root: TreeNode) -> int:
+        if not root : return 0
+        
+        if not root.left and not root.right:
+            return 1  
+        
+        left = self.minDepth(root.left) 
+        right = self.minDepth(root.right) 
+        
+        if not right : return left + 1
+        if not left : return right + 1
+        else : return min(left,right)+1
