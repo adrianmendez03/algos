de42f84ef189cbf62f00d9eef8eca353b1268705
Adrian Mendez <adrianmndz328@gmail.com>
2021-01-31 18:07:36 -0600

    Updated Files

--- python/path_sum.py
+++ python/path_sum.py
@@ -1,13 +1,13 @@
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        if root is None : return False
-        
-        targetSum -= root.val 
-        
-        if root.left is None and root.right is None:
-            return targetSum == 0
-        
-        left = self.hasPathSum(root.left, targetSum)
-        right = self.hasPathSum(root.right, targetSum)
-        
-        return left or right
+def hasPathSum(self, root, targetSum: int):
+    if root is None : return False
+    
+    targetSum -= root.val 
+    
+    if root.left is None and root.right is None:
+        return targetSum == 0
+    
+    left = self.hasPathSum(root.left, targetSum)
+    right = self.hasPathSum(root.right, targetSum)
+    
+    return left or right
         
