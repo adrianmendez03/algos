606b8d367bea62809f7d396d5f389c9841e5e986
Adrian Mendez <adrianmndz328@gmail.com>
2021-02-04 10:09:12 -0600

    Valid Password JavaScript Solution

---
+++ javascript/valid_password.js
@@ -0,0 +1,24 @@
+const validate = (password) => {
+    if (password.length < 6){
+      return false
+    }
+    let upper = false; 
+    let lower = false; 
+    let num = false; 
+    for(let char of password){
+        switch(true){
+            case (char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57):
+                num = true;
+                break;
+            case (char === char.toLowerCase()):
+                lower = true;
+                break;
+            case (char === char.toUpperCase()):
+                upper = true;
+                break;
+            default:
+            break;
+        }
+    }
+    return upper && lower && num; 
+}
