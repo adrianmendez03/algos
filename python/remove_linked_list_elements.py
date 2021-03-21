e4418d48595cf588e7ccd49e36ead0f7e5e75aa1
Adrian Mendez <adrianmndz328@gmail.com>
2021-03-20 22:39:00 -0500

    Remove Linked List Elements Python

---
+++ python/remove_linked_list_elements.py
@@ -0,0 +1,13 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head:
+            if head.val == val:
+                head = self.removeElements(head.next, val)
+            else: 
+                head.next = self.removeElements(head.next, val)
+        return head
