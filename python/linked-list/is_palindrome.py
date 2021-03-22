6bc8500e44f9a6a6f051f53125ad1f780e5c8502
Adrian Mendez <adrianmndz328@gmail.com>
2021-03-22 17:50:58 -0500

    Linked List Palindrome

---
+++ python/linked-list/is_palindrome.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+import collections
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        q = collections.deque()
+        
+        node = head
+        
+        while node != None:
+            q.append(node.val)
+            node = node.next
+        
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+                return False
+            
+        return True
