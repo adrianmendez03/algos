82e911fcea6d262f027dc4e6dc162afb39e6a577
Adrian Mendez <adrianmndz328@gmail.com>
2021-04-21 13:21:25 -0500

    Binary Matrix Leftmost One Javascript

---
+++ javascript/matrix/binary_matrix_leftmost_one.js
@@ -0,0 +1,22 @@
+// You are given a two-dimensional list of integers matrix which contains 1s and 0s. Given that each row is sorted in ascending
+// order with 0s coming before 1s, return the leftmost column index with the value of 1. If there's no row with a 1, return -1.
+
+class Solution {
+    solve(matrix) {
+        if (matrix.length === 0) {
+            return -1
+        }
+        let left = matrix[0].length, found = false
+        for (let i = 0; i < matrix.length; i++) {
+            let j = matrix[i].length - 1
+            while (matrix[i][j] === 1) {
+                j--
+            }
+            if (j + 1 < left) {
+                found = true
+                left = j + 1
+            }
+        }
+        return found ? left : -1
+    }
+}
