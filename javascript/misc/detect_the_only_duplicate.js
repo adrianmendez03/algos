7f026f0bfc790842b1c7bc688e9cbe4fa5734cc2
Adrian Mendez <adrianmndz328@gmail.com>
2021-04-29 12:32:18 -0500

    Detect the Only Duplicate

---
+++ javascript/misc/detect_the_only_duplicate.js
@@ -0,0 +1,23 @@
+// You are given a list nums of length n + 1 picked from the range 1, 2, ..., n. By the pigeonhole principle, 
+// there must be a duplicate. Find and return it. There is guaranteed to be exactly one duplicate.
+
+// Bonus: Can you do this in \mathcal{O}(n)O(n) time and \mathcal{O}(1)O(1) space?
+
+class Solution {
+    solve(nums) {
+        // const s = new Set()
+        // for (let num of nums) {
+        //     if (s.has(num)) {
+        //         return num
+        //     } 
+        //     s.add(num)
+        // }
+
+        // nums = nums.sort((a, b) => a - b) 
+        // for (let i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] === nums[i + 1]) {
+        //         return nums[i]
+        //     }
+        // }
+    }
+}
